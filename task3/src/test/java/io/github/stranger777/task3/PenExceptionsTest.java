a19c7d5c0edcd1c2050a1fb551425cbf20a8be25
Ivan Novikov <noviko-iva-money@yandex.ru>
2018-11-06 10:37:05 +0300

    Update PenExceptionsTest.java

--- task3/src/test/java/io/github/stranger777/task3/PenExceptionsTest.java
+++ task3/src/test/java/io/github/stranger777/task3/PenExceptionsTest.java
@@ -2,8 +2,10 @@ package io.github.stranger777.task3;
 
 import org.junit.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.AfterEach;
 
 public class PenExceptionsTest {
+    Pen illegalPen = null;
 
     static final int NEGATIVE_INK = -1;
     static final double NEGATIVE_SIZE = -1.0;
@@ -12,22 +14,27 @@ public class PenExceptionsTest {
 
     @Test(expected = IllegalArgumentException.class)
    public void pen_inkNegative_throwIllArgException(){
-        Pen illegalPen = new Pen(NEGATIVE_INK);
+        illegalPen = new Pen(NEGATIVE_INK);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void pen_sizeNegative_throwIllArgException(){
-        Pen illegalPen = new Pen( PenTest.PEN_DEFAULT_INK, NEGATIVE_SIZE );
+        illegalPen = new Pen( PenTest.PEN_DEFAULT_INK, NEGATIVE_SIZE );
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void pen_nothingColor_throwIllArgException(){
-        Pen illegalPen = new Pen( PenTest.PEN_DEFAULT_INK, PenTest.PEN_DEFAULT_SIZE_LETTER, NOTHING_COLOR );
+        illegalPen = new Pen( PenTest.PEN_DEFAULT_INK, PenTest.PEN_DEFAULT_SIZE_LETTER, NOTHING_COLOR );
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void pen_zeroLetterSize_throwIllArgException(){
-        Pen illegalPen = new Pen( PenTest.PEN_DEFAULT_INK, ZERO_SIZE, PenTest.PEN_DEFAULT_COLOR );
+        illegalPen = new Pen( PenTest.PEN_DEFAULT_INK, ZERO_SIZE, PenTest.PEN_DEFAULT_COLOR );
+    }
+    
+    @AfterEach
+    tearDown(){
+        illegalPen = null;
     }
 
 
